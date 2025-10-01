b722e3f7b4f8977587b0b1a96dff788576dc064e
Thiago Mota <thiagomota2805@gmail.com>
2025-10-01 12:12:55 -0300

    depositar vi

---
+++ scr/ver_saldo.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = 'Localhost',
+    port = '3307',
+    user = 'root',
+    password = 'root',
+    database = 'banco'
+)
+
+
+def ver_saldo(login, senha, janela):
+    mycursor = mydb.cursor()
+
+    login_final = login.get()
+    senha_final = senha.get()
+
+    try:
+        query = ("SELECT id, login, senha, saldo FROM usuarios WHERE login = %s")
+
+        mycursor.execute(query, (login_final,))
+
+        usuario = mycursor.fetchone()
+
+        saldo = usuario[3]
+
+        print(usuario)
+
+    except:
+        pass
+
+    if usuario == None or usuario == "":
+        txt = tk.Message(janela, text="Erro ao logar! Tente novamente.", fg = "red")
+        txt.grid(row=4, column=1)
+        txt.after(2000, txt.destroy)
+
+    elif usuario[2] != senha_final:
+        txt = tk.Message(janela, text="Erro ao logar! Tente novamente.", fg = "red")
+        txt.grid(row=4, column=1)
+        txt.after(2000, txt.destroy)   
+
+    else:         
+        txt = tk.Message(janela, text= f"Seu saldo é de R${saldo}.")
+        txt.grid(row=4, column=1)
+
+def logar():
+    janela = tk.Tk()
+    janela.geometry("600x300")
+
+    tk.Label(janela, text="Por questão de segurança.", fg="red").grid(row=0, column=1)
+
+    tk.Label(janela, text="Digite o Login").grid(row=1, column=0)
+    login = tk.Entry(janela)
+    login.grid(row=1, column=1)
+
+    tk.Label(janela, text="Digite a senha").grid(row=2, column=0)
+    senha = tk.Entry(janela)
+    senha.grid(row=2, column=1)
+
+    tk.Button(janela, text="Logar", command=lambda: ver_saldo(login, senha, janela)).grid(row=3, column=1)
+
+    janela.mainloop()
+
+
+logar()
+
+
